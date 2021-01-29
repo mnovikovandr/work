682a0e13dd06b7de6fa71e5c2d86c124044884cf
mnovikovandr <misha.novikau@gmail.com>
2021-01-29 09:25:42 +0000

    change scripts

--- startstopscript.sh
+++ startstopscript.sh
@@ -4,15 +4,22 @@
 
 if [[ $1 == run ]];
 then
-  docker run -d --rm --name web -p 8443:443 web	
+  docker ps -a
+  sleep 5
+  docker run -d --rm --name web -p 8443:443 web
+  docker ps 
+  sleep 5
+  echo "-----------------------------------------------------------"
+  echo "curl https://mnovikov.westeurope.cloudapp.azure.com:8443"
+  curl https://mnovikov.westeurope.cloudapp.azure.com:8443 
+  sleep 5
+  echo "-----------------------------------------------------------"
+  echo "curl https://mnovikov.westeurope.cloudapp.azure.com:8443 -k"
+  curl https://mnovikov.westeurope.cloudapp.azure.com:8443 -k
 elif [[ $1 == stop ]];
 then
 docker stop web
+echo "docker stopped"
 else    
 	echo "No Match"
 fi
-
-echo $1
-
-
-# docker run -d --rm --name web -p 8443:443 ssl_nginx
